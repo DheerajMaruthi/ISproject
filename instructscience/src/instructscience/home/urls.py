6944422b42b94181082d7c4371ad4659a056c0d4
IS <dheeraj@webenza.com>
2020-11-30 22:23:37 +0530

    [2020-11-30 22:23:37] added inspire url

--- instructscience/src/instructscience/home/urls.py
+++ instructscience/src/instructscience/home/urls.py
@@ -5,5 +5,5 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
-    path('woi/', views.WoiView.as_view(), name='woi'),
+    path('woi/', views.WoiView.as_view(), name='inspire'),
     ]
